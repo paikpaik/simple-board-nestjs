b150a90d27b65471921282eea0ba9b440dcad26b
paikpaik <woduf9311@gmail.com>
2023-11-01 18:40:08 +0900

    Fix: delete cr 에러 해결 및 boards 코드 수정

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -9,49 +9,52 @@ import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class BoardsService {
-  constructor(private boardRepository: BoardRepository){}
-  
+  constructor(private boardRepository: BoardRepository) {}
+
   // 전체 게시물 조회
-  async getAllBoards(
-    user: User
-  ): Promise<Board[]> {
+  async getAllBoards(user: User): Promise<Board[]> {
     const query = this.boardRepository.createQueryBuilder('board');
     query.where('board.userId = :userId', { userId: user.id });
     const boards = await query.getMany();
 
-    return boards
+    return boards;
   }
-  
+
   // 게시물 생성
   createBoard(createBoardDto: CreateBoardDto, user: User): Promise<Board> {
-    return this.boardRepository.createBoard(createBoardDto, user)
+    return this.boardRepository.createBoard(createBoardDto, user);
   }
-  
+
   // id로 게시물 조회
   async getBoardById(id: number): Promise<Board> {
-    const found = await this.boardRepository.findOne({ where: { id } });
+    const board = await this.boardRepository.findOne({ where: { id } });
 
-    if(!found){
-      throw new NotFoundException(`Cant't find Board with id ${id}`)
+    if (!board) {
+      throw new NotFoundException(
+        `해당 id(${id})의 게시물을 찾을 수 없습니다.`,
+      );
     }
 
-    return found;
+    return board;
   }
-  
+
   // id로 게시물 삭제
   async deleteBoard(id: number, user: User): Promise<void> {
-    const result = await this.boardRepository.delete({id, user:{id:user.id}});
+    const result = await this.boardRepository.delete({
+      id,
+      user: { id: user.id },
+    });
 
-    if(result.affected === 0){
-      throw new NotFoundException(`Can't find Board with id ${id}`)
+    if (result.affected === 0) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
     }
   }
-  
+
   // id로 게시물 상태 변경 (public or private)
   async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
-      const board = await this.getBoardById(id);
-      board.status = status;
-      await this.boardRepository.save(board);
-      return board;
-    }
+    const board = await this.getBoardById(id);
+    board.status = status;
+    await this.boardRepository.save(board);
+    return board;
+  }
 }
